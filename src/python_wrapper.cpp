49f335f6ceec84a1dc7d46319bc35fb76bc244a3
Stuart Berg <bergs@janelia.hhmi.org>
2016-07-19 17:00:36 +0200

    python: Renamed march() to marching_cubes::marching_cubes_impl

--- src/python_wrapper.cpp
+++ src/python_wrapper.cpp
@@ -13,54 +13,59 @@ namespace py = pybind11;
 const int ISO_LEVEL = 1;
 
 
-std::tuple<py::array, py::array, py::array> march(py::array_t<int> volume, int smooth_rounds)
+namespace marching_cubes
 {
-	auto buffer = volume.request();
 
-	if (buffer.ndim != 3)
-	{
-		throw py::value_error("Volume ndim must be 3, not " + buffer.ndim);
-	}
+    std::tuple<py::array, py::array, py::array> marching_cubes_impl(py::array_t<int> volume, int smooth_rounds)
+    {
+        auto buffer = volume.request();
 
-	size_t x_shape = buffer.shape[0];
-	size_t y_shape = buffer.shape[1];
-	size_t z_shape = buffer.shape[2];
+        if (buffer.ndim != 3)
+        {
+            throw py::value_error("Volume ndim must be 3, not " + buffer.ndim);
+        }
 
-	int* volume_ptr = (int*)buffer.ptr;
+        size_t x_shape = buffer.shape[0];
+        size_t y_shape = buffer.shape[1];
+        size_t z_shape = buffer.shape[2];
 
-	Mesh mesh;
+        int* volume_ptr = (int*)buffer.ptr;
 
-	{
-		py::gil_scoped_release release;
-		mesh = march(volume_ptr, x_shape, y_shape, z_shape, ISO_LEVEL);
-	}
+        Mesh mesh;
 
-	if (smooth_rounds > 0)
-	{
-		py::gil_scoped_release release;
-		smooth(mesh, smooth_rounds);
-	}
+        {
+            py::gil_scoped_release release;
+            mesh = march(volume_ptr, x_shape, y_shape, z_shape, ISO_LEVEL);
+        }
+
+        if (smooth_rounds > 0)
+        {
+            py::gil_scoped_release release;
+            smooth(mesh, smooth_rounds);
+        }
 
 
-	std::string vert_format = py::format_descriptor<float>::value;
-	auto vert_info = py::buffer_info(mesh.vertices, sizeof(float), vert_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
-	auto vert_array = py::array(vert_info);
+        std::string vert_format = py::format_descriptor<float>::value;
+        auto vert_info = py::buffer_info(mesh.vertices, sizeof(float), vert_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
+        auto vert_array = py::array(vert_info);
 
-	std::string norm_format = py::format_descriptor<float>::value;
-	auto norm_info = py::buffer_info(mesh.normals, sizeof(float), norm_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
-	auto norm_array = py::array(norm_info);
+        std::string norm_format = py::format_descriptor<float>::value;
+        auto norm_info = py::buffer_info(mesh.normals, sizeof(float), norm_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
+        auto norm_array = py::array(norm_info);
 
-	std::string face_format = py::format_descriptor<unsigned int>::value;
-	auto face_info = py::buffer_info(mesh.faces, sizeof(size_t), face_format, 2, { mesh.faceCount, 3 }, { sizeof(size_t) * 3, sizeof(size_t) });
-	auto face_array = py::array(face_info);
+        std::string face_format = py::format_descriptor<unsigned int>::value;
+        auto face_info = py::buffer_info(mesh.faces, sizeof(size_t), face_format, 2, { mesh.faceCount, 3 }, { sizeof(size_t) * 3, sizeof(size_t) });
+        auto face_array = py::array(face_info);
 
-	return std::make_tuple(vert_array, norm_array, face_array);
+        return std::make_tuple(vert_array, norm_array, face_array);
+    }
+
 }
 
 PYBIND11_PLUGIN(marching_cubes) {
 	py::module m("marching_cubes");
 
-	m.def("march", &march, "Marching cubes implementation");
+	m.def("march", &marching_cubes::marching_cubes_impl, "Marching cubes implementation");
 
 	return m.ptr();
 }
