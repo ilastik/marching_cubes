045aa7ebaffcc270643a2d54de6e5df7238ee25d
Stuart Berg <bergs@janelia.hhmi.org>
2017-07-06 17:41:16 +0200

    conda-recipe: Allow python 3

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,26 +2,34 @@ export CXXFLAGS=""
 export CFLAGS=""
 export LDFLAGS=""
 
+PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
+PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
+PY_ABI=${PY_VER}${PY_ABIFLAGS}
+
 # Depending on our platform, shared libraries end with either .so or .dylib
-if [[ `uname` == 'Darwin' ]]; then
+if [[ $(uname) == 'Darwin' ]]; then
     DYLIB_EXT=dylib
+    CC=clang
+    CXX=clang++
+    CXXFLAGS="${CFLAGS} -std=c++11 -stdlib=libc++"
 else
     DYLIB_EXT=so
+    CC=gcc
+    CXX=g++
+    CXXFLAGS="${CFLAGS} -std=c++11"
 fi
-
-CXXFLAGS="${CFLAGS} -std=c++11"
 
 mkdir build
 cd build
 cmake ..\
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/gcc \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/g++ \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
     -DPYTHON_EXECUTABLE=${PYTHON} \
-    -DPYTHON_LIBRARY=${PREFIX}/lib/libpython2.7.${DYLIB_EXT} \
-    -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7 \
+    -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_ABI}.${DYLIB_EXT} \
+    -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_ABI} \
 ##
 
 make -j${CPU_COUNT}
@@ -29,4 +37,4 @@ make -j${CPU_COUNT}
 # No install step yet.  Copy manually
 #make install
 
-cp ${SRC_DIR}/build/marching_cubes.so ${PREFIX}/lib/python2.7/site-packages/
+cp ${SRC_DIR}/build/marching_cubes*.so ${PREFIX}/lib/python${PY_VER}/site-packages/
