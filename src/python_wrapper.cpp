433e109750fe7bc26f4c705b5c555d93e5bb4844
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-01-25 19:11:28 +0100

    santized repo

--- src/python_wrapper.cpp
+++ src/python_wrapper.cpp
@@ -1,5 +1,14 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
+
+
+
+#include "xtensor/xtensor.hpp"
+#include "xtensor/xbuilder.hpp"
+#include "xtensor/xmath.hpp"              // xtensor import for the C++ universal functions
+#define FORCE_IMPORT_ARRAY
+#include "xtensor-python/pyarray.hpp"     // Numpy bindings
+#include "xtensor-python/pytensor.hpp"     // Numpy bindings
+
 
 #include "marching_cubes.h"
 #include "laplacian_smoothing.h"
@@ -16,20 +25,18 @@ const int ISO_LEVEL = 1;
 namespace marching_cubes
 {
 
-    std::tuple<py::array, py::array, py::array> marching_cubes_impl(py::array_t<int> volume, int smooth_rounds)
+    auto
+    marching_cubes_impl(
+        xt::pytensor<int, 3> volume,
+        int smooth_rounds
+    )
     {
-        auto buffer = volume.request();
+        const auto shape = volume.shape();
+        const size_t x_shape = shape[0];
+        const size_t y_shape = shape[1];
+        const size_t z_shape = shape[2];
 
-        if (buffer.ndim != 3)
-        {
-            throw py::value_error("Volume ndim must be 3, not " + buffer.ndim);
-        }
-
-        size_t x_shape = buffer.shape[0];
-        size_t y_shape = buffer.shape[1];
-        size_t z_shape = buffer.shape[2];
-
-        int* volume_ptr = (int*)buffer.ptr;
+        int* volume_ptr = &volume(0);
 
         Mesh mesh;
 
@@ -45,27 +52,43 @@ namespace marching_cubes
         }
 
 
-        std::string vert_format = py::format_descriptor<float>::value;
-        auto vert_info = py::buffer_info(mesh.vertices, sizeof(float), vert_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
-        auto vert_array = py::array(vert_info);
+        // vertices
 
-        std::string norm_format = py::format_descriptor<float>::value;
-        auto norm_info = py::buffer_info(mesh.normals, sizeof(float), norm_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
-        auto norm_array = py::array(norm_info);
 
-        std::string face_format = py::format_descriptor<unsigned int>::value;
-        auto face_info = py::buffer_info(mesh.faces, sizeof(size_t), face_format, 2, { mesh.faceCount, 3 }, { sizeof(size_t) * 3, sizeof(size_t) });
-        auto face_array = py::array(face_info);
 
-        return std::make_tuple(vert_array, norm_array, face_array);
+        const auto zerosVert = xt::zeros<float >({ int(mesh.vertexCount), int(3) });
+        xt::pytensor<float,  2> vertices = xt::zeros<float >({ int(mesh.vertexCount), int(3) });
+        xt::pytensor<float,  2> normals  = xt::zeros<float >({ int(mesh.vertexCount), int(3) });
+        xt::pytensor<size_t, 2> faces    = xt::zeros<size_t>({ int(mesh.faceCount),   int(3) });
+
+        // // todo, make this more efficient
+        std::copy(&(mesh.vertices[0][0]), (&(mesh.vertices[0][0]))+mesh.vertexCount,&vertices(0));
+        std::copy(&mesh.normals[0][0],  (&mesh.normals[0][0])+mesh.vertexCount, &normals(0));
+        std::copy(&mesh.faces[0],  (&mesh.faces[0])+mesh.faceCount,       &faces(0));
+
+
+
+        // // std::string vert_format = py::format_descriptor<float>::value;
+        // // auto vert_info = py::buffer_info(mesh.vertices, sizeof(float), vert_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
+        // // auto vert_array = py::array(vert_info);
+
+        // // std::string norm_format = py::format_descriptor<float>::value;
+        // // auto norm_info = py::buffer_info(mesh.normals, sizeof(float), norm_format, 2, { mesh.vertexCount, 3 }, { sizeof(float) * 3, sizeof(float) });
+        // // auto norm_array = py::array(norm_info);
+
+        // // std::string face_format = py::format_descriptor<unsigned int>::value;
+        // // auto face_info = py::buffer_info(mesh.faces, sizeof(size_t), face_format, 2, { mesh.faceCount, 3 }, { sizeof(size_t) * 3, sizeof(size_t) });
+        // // auto face_array = py::array(face_info);
+
+        // return std::make_tuple(vertices, normals, faces);
     }
 
 }
 
-PYBIND11_PLUGIN(marching_cubes) {
-	py::module m("marching_cubes");
+PYBIND11_MODULE(marching_cubes,m) {
+	//py::module m("marching_cubes");
 
 	m.def("march", &marching_cubes::marching_cubes_impl, "Marching cubes implementation");
 
-	return m.ptr();
+	//return m.ptr();
 }
